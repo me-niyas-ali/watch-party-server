ae94dd2664c4a788578d3f07a626a203f530774a
Niyas Ali <136828928+me-niyas-ali@users.noreply.github.com>
2026-01-21 19:47:09 +0400

    Update server.js

--- server.js
+++ server.js
@@ -4,34 +4,41 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http, { cors: { origin: "*" } });
 
 const PORT = process.env.PORT || 3000;
-
-app.use(express.static('public')); // serve HTML in "public" folder
+app.use(express.static('public'));
 
 const rooms = {};
 
 io.on('connection', socket => {
-  console.log('New client connected');
+  console.log('New connection:', socket.id);
 
   socket.on('joinRoom', room => {
     socket.join(room);
-    if(!rooms[room]) rooms[room] = new Set();
-    rooms[room].add(socket.id);
+    if(!rooms[room]) rooms[room] = [];
+    rooms[room].push(socket.id);
 
-    io.to(room).emit('updateCount', rooms[room].size);
-    console.log(`Room ${room} connected: ${rooms[room].size}`);
+    // Send all users to new user
+    const otherUsers = rooms[room].filter(id => id !== socket.id);
+    socket.emit('allUsers', otherUsers);
+
+    // Broadcast count
+    io.to(room).emit('updateCount', rooms[room].length);
   });
 
-  socket.on('play', data => socket.to(data.room).emit('play'));
-  socket.on('pause', data => socket.to(data.room).emit('pause'));
-  socket.on('videoURL', data => socket.to(data.room).emit('videoURL', data));
-  socket.on('timeSync', data => socket.to(data.room).emit('timeSync', data));
+  // WebRTC signaling
+  socket.on('offer', payload => io.to(payload.target).emit('offer', { sdp: payload.sdp, callerID: socket.id }));
+  socket.on('answer', payload => io.to(payload.target).emit('answer', { sdp: payload.sdp, callerID: socket.id }));
+  socket.on('ice-candidate', payload => io.to(payload.target).emit('ice-candidate', { candidate: payload.candidate, id: socket.id }));
+
+  // Play/Pause
+  socket.on('play', data => io.to(data.room).emit('play'));
+  socket.on('pause', data => io.to(data.room).emit('pause'));
 
   socket.on('disconnect', () => {
-    for(let room in rooms){
-      rooms[room].delete(socket.id);
-      io.to(room).emit('updateCount', rooms[room].size);
+    for(const room in rooms){
+      rooms[room] = rooms[room].filter(id => id !== socket.id);
+      io.to(room).emit('updateCount', rooms[room].length);
     }
   });
 });
 
-http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+http.listen(PORT, () => console.log('Server running on port', PORT));
