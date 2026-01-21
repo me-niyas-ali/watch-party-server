69518d79811145b01c0047386b2741610f75b755
Niyas Ali <136828928+me-niyas-ali@users.noreply.github.com>
2026-01-21 19:31:23 +0400

    Refactor server.js to use Socket.IO with Express

--- server.js
+++ server.js
@@ -1,41 +1,37 @@
-// server.js
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: process.env.PORT || 3000 });
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, { cors: { origin: "*" } });
 
-const rooms = {}; // { roomCode: Set of clients }
+const PORT = process.env.PORT || 3000;
 
-wss.on('connection', ws => {
-  ws.roomCode = null;
+app.use(express.static('public')); // serve HTML in "public" folder
 
-  ws.on('message', msg => {
-    const data = JSON.parse(msg);
+const rooms = {};
 
-    if (data.type === 'join') {
-      const code = data.room;
-      ws.roomCode = code;
-      if (!rooms[code]) rooms[code] = new Set();
-      rooms[code].add(ws);
+io.on('connection', socket => {
+  console.log('New client connected');
 
-      const count = rooms[code].size;
-      rooms[code].forEach(c => c.send(JSON.stringify({ type: 'count', count })));
-    }
-    else if (['offer','answer','candidate'].includes(data.type)) {
-      const code = ws.roomCode;
-      if (!rooms[code]) return;
-      rooms[code].forEach(c => {
-        if (c !== ws) c.send(JSON.stringify(data));
-      });
-    }
+  socket.on('joinRoom', room => {
+    socket.join(room);
+    if(!rooms[room]) rooms[room] = new Set();
+    rooms[room].add(socket.id);
+
+    io.to(room).emit('updateCount', rooms[room].size);
+    console.log(`Room ${room} connected: ${rooms[room].size}`);
   });
 
-  ws.on('close', () => {
-    if (ws.roomCode && rooms[ws.roomCode]) {
-      rooms[ws.roomCode].delete(ws);
-      const count = rooms[ws.roomCode].size;
-      rooms[ws.roomCode].forEach(c => c.send(JSON.stringify({ type: 'count', count })));
-      if (rooms[ws.roomCode].size === 0) delete rooms[ws.roomCode];
+  socket.on('play', data => socket.to(data.room).emit('play'));
+  socket.on('pause', data => socket.to(data.room).emit('pause'));
+  socket.on('videoURL', data => socket.to(data.room).emit('videoURL', data));
+  socket.on('timeSync', data => socket.to(data.room).emit('timeSync', data));
+
+  socket.on('disconnect', () => {
+    for(let room in rooms){
+      rooms[room].delete(socket.id);
+      io.to(room).emit('updateCount', rooms[room].size);
     }
   });
 });
 
-console.log('WebSocket signaling server running...');
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
